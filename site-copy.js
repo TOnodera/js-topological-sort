09b8f7634c401a1998f6693b59f1cb6c5be29fc2
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-05 13:44:42 +0000

    例をそのままコピー

---
+++ site-copy.js
@@ -0,0 +1,51 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+    }
+}
+
+const graph = function() {
+    const g = new Graph();
+    ["A", "B", "C", "D", "E"].forEach((v) => g.addVertex(v));
+    g.addEdge("A", "B");
+    g.addEdge("A", "C");
+    g.addEdge("B", "D");
+    g.addEdge("C", "D");
+    g.addEdge("D", "E");
+    return g;
+}();
+
+function dfsTopSortHelper(v, n, visited, topNums) {
+    visited[v] = true;
+    const neighbors = graph.adjacencyList[v];
+    for (const neighbor of neighbors) {
+        if (!visited[neighbor]) {
+            n = dfsTopSortHelper(neighbor, n, visited, topNums);
+        }
+    }
+    topNums[v] = n;
+    return n - 1;
+}
+
+function dfsTopSort(graph) {
+    const vertices = Object.keys(graph.adjacencyList);
+    const visited = {};
+    const topNums = {};
+    let n = vertices.length - 1;
+    for (const v of vertices) {
+        if (!visited[v]) {
+            n = dfsTopSortHelper(v, n, visited, topNums)
+        }
+    }
+    return topNums;
+}
+
+console.log(dfsTopSort(graph));
