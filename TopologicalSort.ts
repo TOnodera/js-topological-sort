b298c43a8d54305cc9c435b93a826ec1b6ea52a8
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-10 04:54:14 +0000

    うまくいったぽい

--- TopologicalSort.ts
+++ TopologicalSort.ts
@@ -4,24 +4,24 @@ class TopologicalSort{
     
     static sort(lists: any[]){
 
-        //入次数を確認
-        for(const list of lists){
-            const num = list.parents.length;
-            this.inDeg.set(list.hash,num);
-        }
 
-        //与えられたグラフ内に親がいない場合は-1
-        const parentHashes: any[] = [];
+        //parentsのhashを全て確認
+        const hashes: any[] = [];
         for(const list of lists){
             list.parents.forEach((parentHash: string)=>{
-                parentHashes.push(parentHash);
+                hashes.push(parentHash);
             });
         }
+
+        //入次数を確認
         for(const list of lists){
-            if(parentHashes.includes(list.hash) === false){
-                const num = this.inDeg.get(list.hash);
-                this.inDeg.set(list.hash,num-1);
-            }
+            let inNum = 0;
+            hashes.forEach((parentHash: string)=>{
+                if(list.hash === parentHash){
+                    inNum++;
+                }
+            });
+            this.inDeg.set(list.hash,inNum);
         }
 
         //入次数０のノードを計算
@@ -31,25 +31,23 @@ class TopologicalSort{
             }
         }
 
-
         //ソートして返す
         const ans = [];
         while(this.stack.length > 0){
-            const parentId = this.stack.pop();
-            ans.push(parentId);
-            lists.forEach((list)=>{
-                console.log(parentId,list.parents);
-                if(list.parents.includes(parentId)){
-                    const num = this.inDeg.get(list.hash);
-                    this.inDeg.set(list.hash,num-1);
-                    if(this.inDeg.get(list.hash) === 0){
-                        this.stack.push(list.hash);
-                    }
+            const childHash = this.stack.pop();
+            ans.push(childHash);
+            const currentNode = lists.find((list: any)=>list.hash === childHash);
+            //隣接ノードをみる
+            currentNode.parents.forEach((parentHash: any)=>{
+                const num = this.inDeg.get(parentHash);
+                this.inDeg.set(parentHash,num - 1);
+                if(this.inDeg.get(parentHash) === 0){
+                    this.stack.push(parentHash);
                 }
             });
         }
 
-        return ans;
+        return ans.reverse();
     }
 }
 
